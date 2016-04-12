614b3435bb305a9be38c55c05efe29fdcd23fdda
Zhe <zz921202@gmail.com>
2016-04-11 23:04:36 -0500

    0.2

--- mymnist.py
+++ mymnist.py
@@ -103,4 +103,5 @@ for i in range(0):
     train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
 
-print 'test accuracy %s' % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+print 'test accuracy %s' % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
+
